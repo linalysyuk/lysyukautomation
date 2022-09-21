ea877f836080ea4943863117bf43be231f6501d2
Lina Lysiuk <linalysyuk16@gmail.com>
2022-09-22 01:43:54 +0300

    Hometask11_1.cy.js

--- cypress/e2e/Hometask11_1.cy.js
+++ cypress/e2e/Hometask11_1.cy.js
@@ -6,9 +6,10 @@ describe ('Authorization' , () => {
         cy.wait(10000);
         cy.get('[class=_2UgQw] button').click();
         cy.wait(1000);
-        cy.get('#input_input_emailInput_SM_ROOT_COMP9').type('linalysyuk16@gmail.com');
-        cy.get('#input_input_passwordInput_SM_ROOT_COMP9').type('1234567vvv**8(?>A');
+        cy.get('#input_input_emailInput_SM_ROOT_COMP9').type('test@gmail.com');
+        cy.get('#input_input_passwordInput_SM_ROOT_COMP9').type('123456ffffff$()_);
         cy.get('[class=recaptcha-checkbox-border]').click();
         cy.get('[class=_1fbEI]').click();
         cy.wait(5000);
-        ///а дальше у меня не ранит cypress....
+        cy.get('[class=_1hHt1] ').should('eq', 'test@gmail.com');
+    });
