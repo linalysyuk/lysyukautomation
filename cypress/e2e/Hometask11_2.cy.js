d17c49c5fc319d123592b9b1485608f2eb3dc670
Lina Lysiuk <linalysyuk16@gmail.com>
2022-09-22 16:29:49 +0300

    Hometask11_2.cy.js

--- cypress/e2e/Hometask11_2.cy.js
+++ cypress/e2e/Hometask11_2.cy.js
@@ -3,10 +3,10 @@ describe('Comment form', () => {
     
       cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event');
       cy.scrollTo('center');
-      cy.wait(50000);
-      cy.get('[data-hook=comment-box-placeholder]').type('test');
-      cy.get('[data-hook=submit-button]').click();
-      cy.get('[data-hook=comments-author]').should('eq', 'test');
+      cy.wait(5000);
+      cy.get('div [class="_xAN4M"]').type('test');
+      cy.get('div [class="sseJy4 s__5IRdP sknm0Z oNm4fr---priority-7-primary oNm4fr--upgrade sE8AOM Ew0dET hd2vqg"]').click();
+      cy.get('[data-hook=comments-author]').invoke('text').should('eq', 'test');
         });
 });
-////не могу заранить на cypress,выдает ошибку,прошу помочь
+
