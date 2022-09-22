d7f9b7de40949ff4a363f7d22314f7fca6933e88
Lina Lysiuk <linalysyuk16@gmail.com>
2022-09-22 16:11:10 +0300

    Hometasl11_1.cy.js

--- cypress/e2e/Hometask11_1.cy.js
+++ cypress/e2e/Hometask11_1.cy.js
@@ -2,14 +2,15 @@ describe ('Authorization' , () => {
     it('', () => { 
         cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/');
         cy.wait(5000);
-        cy.get('[class=_1UDJF]').click();
-        cy.wait(10000);
-        cy.get('[class=_2UgQw] button').click();
-        cy.wait(1000);
-        cy.get('#input_input_emailInput_SM_ROOT_COMP9').type('test@gmail.com');
-        cy.get('#input_input_passwordInput_SM_ROOT_COMP9').type('123456ffffff$()_);
-        cy.get('[class=recaptcha-checkbox-border]').click();
-        cy.get('[class=_1fbEI]').click();
+        cy.get('div [class="_1UDJF"]').click();
         cy.wait(5000);
-        cy.get('[class=_1hHt1] ').should('eq', 'test@gmail.com');
+        cy.get('div [class=_1Qjd7]').click();
+        cy.get('#input_input_emailInput_SM_ROOT_COMP9').type('test@gmail.com');
+        cy.get('#input_input_passwordInput_SM_ROOT_COMP9').type('123456ffffff$');
+        cy.get('div [class=recaptcha-checkbox-border]').click();
+        cy.wait(3000);
+        cy.get('div [class="_1fbEI"]').click();
+        cy.wait(3000);
+        cy.get('[class=_1hHt1] ').should('eq','test@gmail.com');
     });
+});
