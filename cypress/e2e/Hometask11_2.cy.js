e76b96981d700e2528d5e92cba73c2d2115c845b
shadowlightcode <59807230+shadowlightcode@users.noreply.github.com>
2022-09-26 19:05:43 +0300

    check homework 11

--- cypress/e2e/Hometask11_2.cy.js
+++ cypress/e2e/Hometask11_2.cy.js
@@ -1,3 +1,37 @@
+/*Comments
+1)  поменяй ссылку и тест начнет работать и проходить - https://yevheniiahlovatska.editorx.io/web-practice/post/manage-your-blog-from-your-live-site
+вот примеры как еще сделали
+describe ('Добавление комментария' , () => {
+   it('Авторизация сразу+удаление', () => {
+    cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event').viewport('ipad-mini');
+    cy.wait(9000);
+    cy.get('[data-testid="section-container"] button').eq(0).click();
+    Cypress.on('uncaught:exception', (err) => { //ошибка Error: ResizeObserver loop limit exceeded
+    return false;
+     });
+    cy.get('[data-testid="signUp.switchToSignUp"]').click();
+    cy.get('[id*="Email"] button').click();
+    cy.get('[type="email"]').type('fortestonly@mail.com', {delay : 20});
+    cy.get('[type="password"]').type('Qwerty123*', {delay : 20});
+    cy.get('[data-testid="submit"] button').click();
+    cy.wait(5000);
+    cy.get('[data-hook="recent-posts"]').scrollIntoView();
+    cy.get('[data-hook="comment-box-placeholder"]').eq(0).click();
+    cy.get('[aria-describedby="placeholder-editor"]').type('comment1');
+    cy.get('[data-hook="submit-button"]').click();
+    cy.get('#viewer-foo', {timeout : 5000}).eq(0).should('have.text', 'comment1');
+    cy.wait(5000);
+    cy.get('[data-hook="actions-menu-btn"] svg').eq(0).scrollIntoView().click();
+    cy.get('[data-content="Delete"]').click();
+    cy.contains('[data-hook="submit-button"]', 'Delete').click();
+  });
+});
+
+Попробуй реализовать сама с новой ссылкой, и потом сократи пример который я тебе тут скинула.
+
+*/
+
+
 describe('Comment form', () => {
     it('', () => {
     
