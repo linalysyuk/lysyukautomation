0754e92d0c6c618baf522e656f2411aa55d509fd
Lina Lysiuk <linalysyuk16@gmail.com>
2022-10-11 00:57:27 +0300

    HW16.cy.js

---
+++ cypress/e2e/HW16.cy.js
@@ -0,0 +1,61 @@
+//*
+describe('empty spec', () => {
+    it('passes', () => {
+      cy.request({
+        method:'POST',
+        url:'https://restful-booker.herokuapp.com/auth',
+        header:{
+          'Content-Type': 'application/json'
+        },
+        body:{
+          "username" : "admin",
+          "password" : "password123"
+        }
+      }).then((response)=>{
+        const autoToken = response.body;
+        cy.log(autoToken);
+      }).then((response)=>{
+          cy.request({
+          method:'POST',
+          url:'https://restful-booker.herokuapp.com/booking',
+          header:{
+            'Content-Type': 'application/json'
+          },
+          body:{
+            firstname : "Alex",
+            lastname : "Brown",
+            totalprice : 113,
+            depositpaid : true,
+            bookingdates : {
+                checkin : "2022-01-01",
+                checkout : "2023-01-01"
+                      },
+            additionalneeds : "Breakfast"
+          }
+        }).then((response)=>{
+         // expect(response).to.have.property('status', 200);
+          const bookingId = response.body.bookingid;
+          cy.log(bookingId);
+          const autoToken = response.body;
+          cy.request({
+            method:'PUT',
+            url:`https://restful-booker.herokuapp.com/booking/${bookingId}`,
+            header:{
+              'Cookie': `${autoToken}`,
+            },
+            body:{
+              firstname : "Olha",
+             // lastname : "Grifin",
+            //  totalprice : 115,
+             // depositpaid : true,
+             // bookingdates : {
+             // checkin : "2023-01-01",
+             // checkout : "2024-01-01"
+      },
+            // additionalneeds : "Breakfast"
+         //   }
+          })
+        })
+      })
+    })
+  })
